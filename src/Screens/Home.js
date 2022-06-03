e0c5ad0529b89884c52e76ac0b14ae09ea4fab7d
Ravinder <ravinder.s.negi@indusnet.co.in>
2022-06-03 17:07:38 +0530

    Error free

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,33 +1,18 @@
-import {View, Text, Button,StyleSheet} from 'react-native';
-import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import React from 'react';
 import MusicPlayer from '../components/MusicPlayer';
-import BottomSheet from '../components/BottomSheet';
-import {Provider} from 'react-native-paper';
 const Home = () => {
-  const [show, setShow] = useState(true);
   return (
-    <Provider>
-      <View style={styles.container}>
-        {/* <MusicPlayer /> */}
-        <Button title="show sheet" onPress={() => setShow(true)} />
-        < 
-          show={show}
-          onDismiss={() => {
-            setShow(false);
-          }}
-          enableBackdropDismiss
-          MusicPlayer={MusicPlayer}
-        />
-      </View>
-    </Provider>
+    <View style={styles.container}>
+      <MusicPlayer />
+    </View>
   );
 };
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor:"#ccc" 
-  }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default Home;
