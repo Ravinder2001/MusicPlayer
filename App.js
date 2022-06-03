6cbcd7a9e73f93b323aaba58cbd8fb45c17d4eaa
Ravinder <ravinder.s.negi@indusnet.co.in>
2022-06-03 18:00:51 +0530

    final

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import Home from './src/Screens/Home';
-import {View, StatusBar, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import React from 'react';
-
+import MusicPlayer from './src/Screens/MusicPlayer';
+import Search from './src/Screens/Search';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 const App = () => {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      {/* <StatusBar backgroundColor="red" barStyle="default" /> */}
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="MusicPlayer"
+          component={MusicPlayer}
+        />
+        <Tab.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Search"
+          component={Search}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+
 export default App;
