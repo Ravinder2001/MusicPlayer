9ff060df486dfa907eeaf3a41655ddc0c6a0a87a
yash <yash.garg@indusnet.co.in>
2022-06-04 17:27:45 +0530

    Bottom bar

---
+++ src/Components/Navigations.js
@@ -0,0 +1,126 @@
+import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import MusicPlayer from '../Screens/MusicPlayer';
+import Search from '../Screens/Search';
+import Favorite from '../Screens/Favorite';
+import Playlist from '../Screens/Playlist';
+import Settings from '../Screens/Settings';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import LinearGradient from 'react-native-linear-gradient';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const Navigations = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="MusicPlayer"
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: '#030A5C',
+          },
+        }}>
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View style={focused ? styles.focusIcon : styles.unFocusIcon}>
+                <Icon
+                  name="search"
+                  size={35}
+                  style={{color:  'white'}}
+                />
+              </View>
+            ),
+          }}
+          name="Search"
+          component={Search}
+        />
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View style={focused ? styles.focusIcon : styles.unFocusIcon}>
+                <MaterialCommunityIcons
+                  name="playlist-music"
+                  size={35}
+                  style={{color:  'white'}}
+                />
+              </View>
+            ),
+          }}
+          name="Playlist"
+          component={Playlist}
+        />
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View style={focused ? styles.focusIcon : styles.unFocusIcon}>
+                <Icon
+                  name="musical-note"
+                  size={35}
+                  style={{color:  'white'}}
+                />
+              </View>
+            ),
+          }}
+          name="MusicPlayer"
+          component={MusicPlayer}
+        />
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View style={focused ? styles.focusIcon : styles.unFocusIcon}>
+                <Icon
+                  name="heart-sharp"
+                  size={35}
+                  style={{color:  'white'}}
+                />
+              </View>
+            ),
+          }}
+          name="Favorite"
+          component={Favorite}
+        />
+        <Tab.Screen
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+              <View style={focused ? styles.focusIcon : styles.unFocusIcon}>
+                <Icon
+                  name="reorder-three-sharp"
+                  size={35}
+                  style={{color:  'white'}}
+                />
+              </View>
+            ),
+          }}
+          name="Settings"
+          component={Settings}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+const styles = StyleSheet.create({
+  focusIcon: {
+    position: 'absolute',
+    top: -20,
+    backgroundColor: 'red',
+    borderRadius: 50,
+
+    padding: 10,
+  },
+  unFocusIcon: {
+    backgroundColor: '#030A5C',
+  },
+});
+export default Navigations;
